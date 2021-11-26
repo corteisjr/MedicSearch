982342747873d39dd97097cd2a62d881b96f64a5
corteisjr <corteisjunior@gmail.com>
2021-11-26 20:22:02 +0200

    Arquivo com Secret Keys

---
+++ medicSearchAdmin/settings/testing.py
@@ -0,0 +1,14 @@
+import os
+from .settings import *
+
+DEBUG = False
+
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,  'db.sqlite3'),
+    }
+}
