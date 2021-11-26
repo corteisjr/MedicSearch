f00e6b1c4590a979a3ed8f7641518688d6ca9397
corteisjr <corteisjunior@gmail.com>
2021-11-26 18:27:05 +0200

    Adicionando Arquivos

---
+++ medicSearch/models/Profile.py
@@ -0,0 +1,32 @@
+from medicSearch import models
+from medicSearch.models import *
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.IntegerField(choices=ROLE_CHOICE,  default=3)
+    birthday = models.DateField(default=None, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    token = models.CharField(max_length=255,  null=True, blank=True)
+    
+    def __str__(self):
+        return '{}'.format(self.user.username)
+    
+    @receiver(post_save, sender=User)
+    
+    def create_user_profile(sender, instance, created, **kwargs):
+        try:
+            if created:
+                Profile.objects.create(user=instance)
+                
+        except:
+            pass
+        
+    @receiver(post_save, sender=User)
+    def  _save_user_profile(sender, instance,  **kwargs):
+        try:
+             instance.profile.save()        
+        except:
+            pass
+    
---
+++ medicSearch/models/__init__.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+ROLE_CHOICE = (
+    (1, 'Admin'),
+    (2, 'Médico'),
+    (3, 'Paciente'),
+)
+
+from .Profile import Profile
