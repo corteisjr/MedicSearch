6cf0b6e7273ab60a5701dd276fc98bc8dfe5096f
corteisjr <corteisjunior@gmail.com>
2021-11-27 13:44:32 +0200

    Personalização do admin

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
 from  .models import *
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', )
+    # Cria um filtro de hierquia com datas
+    data_hierarchy = 'created_at'
+    #Elementos que desejamos que sejam listados
+    list_display = ('user', 'role', 'birthday', 'specialtiesList', 'addressesList', )
+    
+    def specialtiesList(self, obj):
+        return [i.name for i in obj.specialties.all() ]
+    
+    def addressesList(self, obj):
+        return [i.name for i in obj.addresses.all()]
+    list_display_links = ('user', 'role', )
+    #Apresentação de campos vazios
+    empty_value_display = 'Vazio'
+    #Campos que poderam ser pesquisados
+    search_fields = ('user__username', )
+    
+    fieldsets = (
+        ('Usuário', {
+            'fields': ('user', 'birthday', 'image')
+        }),
+        ('Função', {
+            'fields': ('role', )
+        }),
+        
+        ('Extras', {
+            'fields': ('specialties', 'addresses')
+        }),
+    )
+    
+    def birth(self, obj):
+        return obj.birthday
+    birth.empty_value_display = '___/___/___'
+
+    
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Province)
 admin.site.register(City)
 admin.site.register(Neighborhood)
